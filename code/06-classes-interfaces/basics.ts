ba4bd81bbc74c8c9ce0dee1366bb9b7d621e4d1f
Daria <bunikdar@gmail.com>
2025-09-26 15:00:11 +0200

    add private and public property

--- code/06-classes-interfaces/basics.ts
+++ code/06-classes-interfaces/basics.ts
@@ -10,10 +10,24 @@
 // }
 
 // TS shortcut (alternative version)
+// class User {
+//   constructor(public name: string, public age?: number) {}
+// }
+
+// Public - Private
+
 class User {
-  constructor(public name: string, public age?: number) {}
+  public hobbies: string[] = [];
+  constructor(public name: string, private age?: number) {}
+
+  // here accessing property age is possible, because we do it inside the class
+  greet() {
+    console.log(`Hello ${this.age} Smarty Pants`);
+  }
 }
 
-const daria = new User('Max', 37);
-const jakub = new User('Jakub')
+const daria = new User("Daria", 37);
+const jakub = new User("Jakub");
 
+daria.hobbies = ["tennis", "joga", "graphic design"];
+// daria.age = 56 // accesing private property outside the class is not available
