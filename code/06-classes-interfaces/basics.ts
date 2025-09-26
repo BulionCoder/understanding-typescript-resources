3254c6b5de5e441c49672d406dd6c7d4ea5fe341
Daria <bunikdar@gmail.com>
2025-09-26 14:33:17 +0200

    use shortcut when creating classes

---
+++ code/06-classes-interfaces/basics.ts
@@ -0,0 +1,22 @@
+// class User {
+//   name: string;
+//   age: number;
+
+//   constructor(n: string, a: number) {
+//     // this.name = 'Max';
+//     this.name = n;
+//     this.age = a;
+//   }
+// }
+
+// TS shortcut (alternative version)
+class User {
+  constructor(public name: string, public age?: number) {}
+}
+
+const daria = new User('Max', 37);
+const jakub = new User('Jakub')
+
+
+console.log(daria)
+console.log(jakub)
