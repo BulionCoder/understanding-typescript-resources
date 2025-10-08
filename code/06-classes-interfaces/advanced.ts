ced2ceab9bb441b67381da0dcf29b962ea338671
Daria <bunikdar@gmail.com>
2025-10-08 19:15:23 +0200

    setting values  with setters

--- code/06-classes-interfaces/advanced.ts
+++ code/06-classes-interfaces/advanced.ts
@@ -1,68 +1,106 @@
+// class User {
+//   protected _firstName: string = '';
+//   private _lastName: string = '';
+
+//   set firstName(name: string) {
+//     if (name.trim() === '') {
+//       throw new Error('Invalid name.');
+//     }
+//     this._firstName = name;
+//   }
+
+//   set lastName(name: string) {
+//     if (name.trim() === '') {
+//       throw new Error('Invalid name.');
+//     }
+//     this._lastName = name;
+//   }
+
+//   get fullName() {
+//     return this._firstName + ' ' + this._lastName;
+//   }
+
+//   static eid = 'USER';
+
+//   static greet() {
+//     console.log('Hello!');
+//   }
+// }
+
+// console.log(User.eid);
+// User.greet();
+
+// const max = new User();
+// max.firstName = 'Max';
+// max.lastName = '';
+// // max._firstName = 'Max 2';
+// console.log(max.fullName);
+
+// class Employee extends User {
+//   constructor(public jobTitle: string) {
+//     super();
+//     // super.firstName = 'Max';
+//   }
+
+//   work() {
+//     // ...
+//     console.log(this._firstName);
+//     // super._firstName
+//   }
+// }
+
+// abstract class UIElement {
+//   constructor(public identifier: string) {}
+
+//   clone(targetLocation: string) {
+//     // logic to duplicate the UI element
+//   }
+// }
+
+// // let uiElement = new UIElement();
+
+// class SideDrawerElement extends UIElement {
+//   constructor(public identifier: string, public position: 'left' | 'right') {
+//     super(identifier);
+//   }
+
+//   // ...
+// }
+
+// class User {
+//   constructor(private firstName: string, private lastName: string) {}
+//   get fullName() {
+//     return this.firstName + " " + this.lastName;
+//   }
+// }
+
+// const daria = new User("Daria", "Bunikowska");
+// console.log(daria.fullName);
+
 class User {
-  protected _firstName: string = '';
-  private _lastName: string = '';
+  private _firstName: string = "";
+  private _lastName: string = "";
 
   set firstName(name: string) {
-    if (name.trim() === '') {
-      throw new Error('Invalid name.');
+    if (name.trim() === "") {
+      throw new Error(" Invalid name");
     }
     this._firstName = name;
   }
 
   set lastName(name: string) {
-    if (name.trim() === '') {
-      throw new Error('Invalid name.');
+    if (name.trim() === "") {
+      throw new Error(" Invalid name");
     }
     this._lastName = name;
   }
-
-  get fullName() {
-    return this._firstName + ' ' + this._lastName;
-  }
-
-  static eid = 'USER';
-
-  static greet() {
-    console.log('Hello!');
+  get fullName(){
+    return this._firstName + " " + this._lastName
   }
 }
 
-console.log(User.eid);
-User.greet();
+const daria = new User();
+daria.firstName = "Daria";
+daria.lastName = ""
+console.log(daria.fullName);
 
-const max = new User();
-max.firstName = 'Max';
-max.lastName = '';
-// max._firstName = 'Max 2';
-console.log(max.fullName);
-
-class Employee extends User {
-  constructor(public jobTitle: string) {
-    super();
-    // super.firstName = 'Max';
-  }
-
-  work() {
-    // ...
-    console.log(this._firstName);
-    // super._firstName
-  }
-}
-
-abstract class UIElement {
-  constructor(public identifier: string) {}
-
-  clone(targetLocation: string) {
-    // logic to duplicate the UI element
-  }
-}
-
-// let uiElement = new UIElement();
-
-class SideDrawerElement extends UIElement {
-  constructor(public identifier: string, public position: 'left' | 'right') {
-    super(identifier);
-  }
-
-  // ...
-}
